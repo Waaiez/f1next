4d348a4c0178f951d7b2da045381b81f214c92fb
Waaiez Kinnear <waaiez.kinnear10@gmail.com>
2023-07-22 21:56:53 +0200

    add initial files

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5 my-10 text-center">
+		<div class="space-y-10">
+			<div>
+				<h4 class="h4">LOCATION</h4>
+				<h5 class="h5">CIRCUIT</h5>
+			</div>
+			<h2 class="h2">ITEM starts in</h2>
+			<h1 class="h1">TIME</h1>
+		</div>
+		<ul>
+			<li>FP1</li>
+			<li>FP2</li>
+			<li>FP3</li>
+			<li>Race</li>
+		</ul>
+	</div>
+</div>
